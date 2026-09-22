65f0125f4b13724bc3c55664ee466729bed6e636
Eduardo Antonio Cruz Bolaños <ac138439@gmail.com>
2026-09-22 12:07:43 -0600

    fix: corregir errores de validaciones

--- src/validators/juego.validator.js
+++ src/validators/juego.validator.js
@@ -1,35 +1,34 @@
-import { body, param, validationResult } from "express-validator";
+import {body,param,validationResult} from "express-validator";
 
-const respoderErrores = (req,res,next) =>
-    {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+const responderErrores=(req,res,next)=>{
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
         }
-        next();
-    }
-
-    export const GENEROS_VALIDOS = [ "accion", "rpg", "aventura", "estrategia", "plataformas" ];
-
-    export const PLATAFORMAS_VALIDAS = ["pc", "playstation", "xbox", "nintendo"];
-
-    export const validarCrearJuego =  [
-        body("titulo").trim().isLength({min: 2, max: 60 }).withMessage("El titulo debe tener entre 2 y 60 caracteres"),
-
-        body("genero").trim().toLowerCase().isIn(GENEROS_VALIDOS).withMessage(`Los generos validos son ${GENEROS_VALIDOS}` ),
-
-body("plataforma".trim().toLoweCase().isIn(PLATAFORMAS_VALIDAS).withMessage(`Las plataformas validas son: ${PLATAFORMAS_VALIDAS}`)) ,
-
-body("horasJugadas").optional()isIn({min: 0}).withMessage("Las horas jugadas deben ser un valor booleano"),
-
-body("completado").optional().isBoolean().withMessage("El estado "),
-
-        responderErrores,
-    ];
+}
 
 
-    export const validarJuegoId = [
-        param("id").idMongoId()withMessage("El id del videojuego no es un ObjectId de MongoDB valido"),
 
-        responderErrores,
-    ];
+export const GENEROS_VALIDOS=["accion","rpg","aventura","estrategia","plataformas"];
+export const PLATAFORMAS_VALIDAS=["pc","playstation","xbox","nintendo"];
+
+export const validarCrearJuego = [
+    body("titulo").trim().isLength({min:2,max:60}).withMessage("El titulo debe tener entre 2 y 60 caracteres"),
+    body("genero").trim().toLowerCase().isIn(GENEROS_VALIDOS).withMessage("El género no es válido"),
+    body("plataforma").trim().toLowerCase().isIn(PLATAFORMAS_VALIDAS).withMessage("La plataforma no es válida"),
+    body("horasJugadas").trim().toLowerCase().isInt({min:0}).withMessage("Las horas jugadas deben ser un número entero "),
+    body("completado").optional().isBoolean().withMessage("El campo completado debe ser un valor booleano"),
+];
+
+
+export const validarActualizarJuego=[
+    body("titulo").optional().trim().isLength({min:2,max:60}).withMessage("El titulo debe tener entre 2 y 60 caracteres"),
+    body("genero").optional().trim().toLowerCase().isIn(GENEROS_VALIDOS).withMessage("El género no es válido"),
+    body("plataforma").optional().trim().toLowerCase().isIn(PLATAFORMAS_VALIDAS).withMessage("La plataforma no es válida"),
+    body("horasJugadas").optional().trim().isInt({min:0}).withMessage("Las horas jugadas deben ser un número entero "),
+    body("completado").optional().isBoolean().withMessage("El campo completado debe ser un valor booleano"),
+]
+
+export const validarJuegoId=[
+    param("id").isMongoId().withMessage("El id del videojuego no es un ObjectId de MongoDB válido"),
+    responderErrores,
+];
