3edb3b9ba1fb3f58a72d0d8d7210f7b30a08c120
Eduardo Antonio Cruz Bolaños <ac138439@gmail.com>
2026-09-22 20:35:18 -0600

    refactor: importar la funcion del controlador y la validacion para una ruta del endpoint

--- src/routes/juego.routes.js
+++ src/routes/juego.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { crearJuego, listarJuegos, obtenerJuego, actualizarJuego, eliminarJuego } from "../controllers/juego.controller.js";
-import { validarCrearJuego, validarActualizarJuego, validarJuegoId } from "../validators/juego.validator.js";
+import { crearJuego, listarJuegos, obtenerJuego, actualizarJuego, eliminarJuego, registrarTiempo} from "../controllers/juego.controller.js";
+import { validarCrearJuego, validarActualizarJuego, validarJuegoId, validarRegistrarTiempo } from "../validators/juego.validator.js";
 
 export const juegoRouter = Router();
 
@@ -9,3 +9,6 @@ juegoRouter.get("/", listarJuegos);
 juegoRouter.get("/:id", validarJuegoId, obtenerJuego);
 juegoRouter.patch("/:id", validarJuegoId, validarActualizarJuego, actualizarJuego);
 juegoRouter.delete("/:id", validarJuegoId, eliminarJuego);
+
+// Endpoint para registrar una sesión de tiempo (mínimo 10 minutos)
+juegoRouter.post("/:id/tiempo", validarRegistrarTiempo, registrarTiempo);
